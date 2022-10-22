5995ac59ece14c1b4f099a953b7259e3cb9e273f
amit <amitperane8473@gmail.com>
2022-10-22 12:49:59 +0530

    logic of time wrt task done

--- timeline/src/Components/ClassOne.jsx
+++ timeline/src/Components/ClassOne.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import { Stack, HStack, Box, Text, VStack, Center } from "@chakra-ui/react";
 const ClassOne = ({ stime }) => {
   var width = 200;
+  var decimal = (stime + "").split(".")[1] ? (stime + "").split(".")[1] : 0;
+  stime = Math.floor(stime);
+  console.log(decimal, stime);
   var onecandle = width / 60;
-  var newWidth =0;
+  var newWidth = 0;
   if (stime === 0.3) {
     newWidth = onecandle * 30;
   }
- 
+
   return (
     <HStack spacing="24px">
       <Box
-        w={newWidth ? newWidth : width}
+        w={newWidth ? newWidth : 150}
         // w={width}
         pos={"absolute"}
         // stime * width + 24 * stime
-        left={newWidth ?stime * width + onecandle * 30 : stime * width + 24 * stime}
+        left={
+          newWidth
+            ? onecandle * decimal
+            : stime * width + 24 * stime + decimal * onecandle
+        }
         h="40px"
         bg="yellow.200"
       >
--- timeline/src/Components/Cls.jsx
+++ timeline/src/Components/Cls.jsx
@@ -3,13 +3,13 @@ import ClassOne from "./ClassOne";
 import { Stack, HStack, Box, Text, VStack, Center } from "@chakra-ui/react";
 
 function Cls() {
-  var startTime = [0.3,1, 2, 5, 6, 7, 8, 9, 16];
+  var startTime = [0.5, 1.31, 2.41, 5, 6, 7, 8, 9, 16];
   var time = [
-    0, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23,
+    1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23,
   ];
   return (
-    <HStack  mt={20} pos={"relative"}>
+    <HStack mt={20} pos={"relative"}>
       {startTime.map((stime) => {
         return <ClassOne stime={stime} />;
       })}
